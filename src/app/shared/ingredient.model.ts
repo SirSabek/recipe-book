9f29b643bc37f05c4eaa87011c759a88ee5c198c
SirSabek <mmsabek99@gmail.com>
2023-11-04 15:37:00 +0200

    Initial Commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    // 
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+    // 
+    // The above code can be replaced with the following code:
+    constructor(public name: string, public amount: number) {}
+}
