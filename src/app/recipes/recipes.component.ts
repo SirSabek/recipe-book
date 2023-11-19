509cf88ee9257f12808bf7c16404b38e18c1728a
SirSabek <mmsabek99@gmail.com>
2023-11-19 20:53:21 +0200

    adding a working navigation using event&propertiy binding

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './Recipe.Model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class RecipesComponent {
 
+  constructor() { }
+
+  selectedRecipe!: Recipe;
 }
